06b00372560c4e06dbfcf79a2d87c8533e980298
Alessio <s294856@studenti.polito.it>
2023-04-20 22:39:20 +0000

    updates

--- PFM/files/views.py
+++ PFM/files/views.py
@@ -8,6 +8,6 @@ def LoadFile(request, pk):
     expenses = file.readlines()
     for expense in expenses:
         row = expense.rstrip().split(",")
-        e = Expense(user=request.user, balance=Balance.objects.get(name=row[1]), payment_method=Payment_Method.objects.get(name=row[2]), amount=int(row[3]), date=row[4], category=Category.objects.get(name=row[5]), description=row[6])
+        e = Expense(user=request.user, balance=Balance.objects.get(name=row[1]), payment_method=Payment_Method.objects.get(name=row[2]), amount=float(row[3]), date=row[4], category=Category.objects.get(name=row[5]), description=row[6])
         e.save()
     return redirect('expenses_list')
