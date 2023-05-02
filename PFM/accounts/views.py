e3afd87e8519c3ce8f6a56fcd172d708d318511f
Alessio <s294856@studenti.polito.it>
2023-05-02 22:33:21 +0000

    Update account registation view

--- PFM/accounts/views.py
+++ PFM/accounts/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import (
     authenticate,
-    get_user_model,
     login,
     logout,
 )
 from .forms import UserLoginForm, UserRegisterForm
- 
+
 def login_view(request):
     next = request.GET.get('next')
     title = 'Login'
@@ -20,7 +19,7 @@ def login_view(request):
             return redirect(next)
         return redirect('/')
     return render(request, 'login.html', {'form': form, 'title': title})
- 
+
 def register_view(request):
     next = request.GET.get('next')
     title = 'Registrazione'
@@ -30,13 +29,13 @@ def register_view(request):
         password = form.cleaned_data.get('password')
         user.set_password(password)
         user.save()
-        new_user = authenticate(username=user.username, password=user.password)
+        new_user = authenticate(username=user.username, password=password)
         login(request, new_user)
         if next:
             return redirect(next)
         return redirect('/')
     return render(request, 'account_form.html', {'form': form, 'title': title})
- 
+
 def logout_view(request):
     logout(request)
     return redirect('/')
